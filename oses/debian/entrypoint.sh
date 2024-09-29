d40661304ec4781899f973717ca33aa79d30bd11
Adem <100190744+AdemErsln@users.noreply.github.com>
2024-09-29 17:49:25 +0300

    Update entrypoint.sh

--- oses/debian/entrypoint.sh
+++ oses/debian/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes. 
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+# İç IP adresini al
+export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
-# Replace Startup Variables
+# Başlangıç değişkenlerini değiştir
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
+# Sunucuyu çalıştır
 eval ${MODIFIED_STARTUP}
